ef0749185b59b6e77e4df0436135ff9e27030ef7
Thomas Klemmensen <thomas@kvalitetsit.dk>
2018-09-11 11:46:21 +0200

    Do not fail when WARNINGs

--- eksempel/src/main/java/dk/sts/appointment/services/XdsException.java
+++ eksempel/src/main/java/dk/sts/appointment/services/XdsException.java
@@ -16,11 +16,14 @@ public class XdsException extends Exception {
 	}
 
 	public XdsException(RegistryErrorList registryErrorList) {
-		for (RegistryError error : registryErrorList.getRegistryError()) {
-			addError("Error received from registry [codeContext:"+error.getCodeContext()+", errorCode:"+error.getErrorCode()+", errorValue:"+error.getValue()+"]");
-		}
+		this(new LinkedList<String>(), registryErrorList);
 	}
-
+	
+	private XdsException(List<String> errors, RegistryErrorList registryErrorList) {
+		super(toString(errors, registryErrorList));
+		setErrors(errors);		
+	}
+	
 	public String addError(String error) {
 		this.errors.add(error);
 		return error;
@@ -32,5 +35,29 @@ public class XdsException extends Exception {
 
 	public void setErrors(List<String> errors) {
 		this.errors = errors;
+	}
+	
+	static String toString(List<String> errors, RegistryErrorList list) {
+		StringBuilder result = new StringBuilder();
+		List<RegistryError> registryErrors = list.getRegistryError();
+		String s = "Error received from registry: ";
+		result.append(s);
+		for (RegistryError error : registryErrors) {
+			String err = toString(error);
+			result.append(err);
+			errors.add(s+err);
+		}
+		return result.toString();
+	}
+	
+	static String toString(RegistryError err) {
+		StringBuilder result = new StringBuilder();
+		result.append("[ errorCode:").append(err.getErrorCode());
+		result.append(", codectx:").append(err.getCodeContext());
+		result.append(", location:").append(err.getLocation());
+		result.append(", severity:").append(err.getSeverity());
+		result.append(", value:").append(err.getValue());
+		result.append("]");
+		return result.toString();
 	}
 }
